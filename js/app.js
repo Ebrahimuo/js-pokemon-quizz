41ffb5348220ff7900d59962813fe65b9504af84
Ebrahim User <ebrahimuo@gmail.com>
2025-07-01 16:06:57 +0300

    Fixed problem with end of the quizz

--- js/app.js
+++ js/app.js
@@ -32,36 +32,36 @@ const questionsQuiz1=[{question: "What was the first Pokémon ever created?",
                     options: ["Hop", "Bea", "Raihan"]
                 }];
 
-const questionsQuiz2=[{question: "What was the first Pokémon ever created?",
-                 answer: "Rhydon",
-                 options: ["Pikachu", "Bulbasaur", "Mew"]
-                },{question: "Which Pokémon is known as the “Eon Pokémon”?",
-                    answer: " Latios and Latias",
-                    options: ["Lugia", "Mewtwo", "Rayquaza"]
-                },{question: "What type is the Pokémon “Garchomp”?",
-                    answer: "Dragon/Ground",
-                    options: ["Dragon/Flying", "Ground/Rock", "Dragon/Steel"]
-                },{question: "In which generation was the Fairy-type introduced?",
-                    answer: "Generation VI",
-                    options: ["Generation IV", "Generation V", "Generation VII"]
-                },{question: "Which of the following Pokémon is not a Legendary Pokémon?",
+const questionsQuiz2=[{question: "What is the evolutionary form of Porygon-Z?",
+                 answer: "None, Porygon-Z is the final form",
+                 options: ["Porygon2", "Porygon", "Porygon-X"]
+                },{question: "Which Pokémon’s cry is the longest in the games?",
+                    answer: "Kricketune",
+                    options: ["Jynx", "Wailord", "Palkia"]
+                },{question: "What is the signature move of the Pokémon Aegislash?",
+                    answer: "King’s Shield",
+                    options: [" Sacred Sword", "Iron Head", "Shadow Claw"]
+                },{question: "What is the only Pokémon that can learn the move “Dragon Ascent”?",
+                    answer: "Rayquaza",
+                    options: ["Garchomp", "Dragonite", "Salamence"]
+                },{question: "What are the two types of the Pokémon Giratina in its Altered Forme?",
+                    answer: "Ghost/Dragon",
+                    options: ["Dragon/Flying", "Ghost/Flying", "Ghost/Dark"]
+                },{question: "Which Pokémon has a Gigantamax form that turns into a skyscraper-like structure?",
+                    answer: "Duraludon",
+                    options: ["Coalossal", "Copperajah", "Corviknight"]
+                },{question: "What is the main Legendary Pokémon in Pokémon X?",
+                    answer: "Xerneas",
+                    options: ["Yveltal", "Zygarde", "Diancie"]
+                },{question: "Which Pokémon can Mega Evolve into Mega Garchomp?",
                     answer: "Garchomp",
-                    options: ["Entei", "Regirock", "Cresselia"]
-                },{question: "What is the name of the region in Pokémon Sun and Moon?",
-                    answer: "Alola",
-                    options: ["Sinnoh", "Hoenn", "Unova"]
-                },{question: "Which Pokémon can evolve into multiple different forms depending on certain conditions?",
-                    answer: "Eevee",
-                    options: ["Pikachu", "Mew", "Ditto"]
-                },{question: "What move does the most damage to a Ghost-type Pokémon?",
-                    answer: "Crunch",
-                    options: ["Psychic", "Shadow Ball", "Hyper Beam"]
-                },{question: "Which Pokémon is known for having the signature move “Spacial Rend”?",
-                    answer: "Palkia",
-                    options: ["Dialga", "Giratina", "Arceus"]
-                },{question: "Who is the current champion of the Galar region in Pokémon Sword and Shield?",
-                    answer: "Leon",
-                    options: ["Hop", "Bea", "Raihan"]
+                    options: ["None, Garchomp cannot Mega Evolve", "Gible", "Gabite"]
+                },{question: "Which Pokémon is known as the “Battle Bond” form when it bonds with its Trainer?",
+                    answer: "Ash-Greninja",
+                    options: ["Charizard", "Lucario", "Pikachu"]
+                },{question: "What is the effect of the move “Spore”?",
+                    answer: "Puts the target to sleep",
+                    options: ["Reduces the target’s Speed", "Poisons the target", "Paralyzes the target"]
                 }];
 
 
@@ -91,9 +91,7 @@ const formInputs = document.querySelectorAll("form-check-input");
 
 
 function init(event){
-
-
-
+    event.preventDefault();
     if(event.submitter.id=="prevButton"){ // RUN ONLY ON PREVIOUS BUTTON
         console.log("Inside prevButton submitter");
         if (previousAnswer==questionsQuiz1[questionNumber].answer){
@@ -119,18 +117,17 @@ function init(event){
            }
         log.innerText = score;
         console.log(score);
-        event.preventDefault();
     }
         
     } else {                    //NEXT BUTTON PRESS
         console.log(questionNumber);
         console.log(questionsQuiz1.length);
-        if(questionNumber>=(questionsQuiz1.length-1)){                          //END
+        if(questionNumber==(questionsQuiz1.length-1)){                          //END
             console.log("NO NEXT?")
             questionCount.innerText="---------";
             questionSlot.innerText="Final score is: " + score;
-            // questionNumber++;
-        } else {                                                        //OTHERWISE NOT END
+            questionNumber++;
+        } else if( questionNumber<(questionsQuiz1.length-1)){                                                        //OTHERWISE NOT END
 
         
 
@@ -184,8 +181,14 @@ function init(event){
     console.log(score);
     event.preventDefault();
 }
+else {
+    //what to do if done?
+     radioBtns[j%4].innerText=questionsQuiz1[questionNumber].answer;
+
 }
 }
+}
+
 
 
 
