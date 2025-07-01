5798e6a6596d450505eb0b7fe3e6253cea1a43e6
Ebrahim User <ebrahimuo@gmail.com>
2025-07-01 15:07:07 +0300

    Problems with end of quizz

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-/*-------------- Constants -------------*/
+/*-------------- CONS -------------*/
 
 const questionsQuiz1=[{question: "What was the first Pokémon ever created?",
                  answer: "Rhydon",
                  options: ["Pikachu", "Bulbasaur", "Mew"]
                 },{question: "Which Pokémon is known as the “Eon Pokémon”?",
-                    answer: " Latios and Latias",
+                    answer: "Latios and Latias",
                     options: ["Lugia", "Mewtwo", "Rayquaza"]
                 },{question: "What type is the Pokémon “Garchomp”?",
                     answer: "Dragon/Ground",
@@ -65,52 +65,173 @@ const questionsQuiz2=[{question: "What was the first Pokémon ever created?",
                 }];
 
 
-/*---------- Variables (state) ---------*/
+/*---------- VARS ---------*/
 
 let newGame=true;
-
 let questionNumber = 0;
-
-let trackingQuestion= 0; // the question the computer is at;
-
+let trackingQuestion= 0;
 let score = 0;
+let previousAnswer="";
 
-/*----- Cached Element References  -----*/
+/*-----     CACHE  -----*/
 
 let questionCount = document.getElementById("questionN");
-
 let questionSlot = document.getElementById("questionSlot");
 
-let proceedBtn = document.getElementById("proceedButton");
 
-proceedBtn.addEventListener('click', init)
 
-//radio elements;
 
 let radioBtns = document.querySelectorAll(".form-check-label");
+const formInputs = document.querySelectorAll("form-check-input");
 
 
-/*-------------- Functions -------------*/
+
+
+/*-------------- FUNCTIONS -------------*/
 
 
 function init(event){
 
-    // if quizz 1
-    //update everything
-    //make a switch case for question count:
 
-    questionNumber++;
-    questionCount.innerText=questionNumber;
 
-    questionSlot.innerText = questionsQuiz1[questionNumber].question;
+    if(event.submitter.id=="prevButton"){ // RUN ONLY ON PREVIOUS BUTTON
+        console.log("Inside prevButton submitter");
+        if (previousAnswer==questionsQuiz1[questionNumber].answer){
+            score--;
+            previousAnswer="";
+            console.log("New score after decrementing is: " + score);
+        }
+        questionNumber--;
+        questionCount.innerText=questionNumber+1;
+        
+         questionSlot.innerText = questionsQuiz1[questionNumber].question;
+         for(let i=0; i<4;i++){
+        
+        if(i!=3){
+            radioBtns[j%4].innerText=questionsQuiz1[questionNumber].options[i];
+            j++;
+            console.log("Branch 1");
+            
+        } else {
+            radioBtns[j%4].innerText=questionsQuiz1[questionNumber].answer;
+              console.log("Branch 2");
+            j++;
+           }
+        log.innerText = score;
+        console.log(score);
+        event.preventDefault();
+    }
+        
+    } else {                    //NEXT BUTTON PRESS
+        console.log(questionNumber);
+        console.log(questionsQuiz1.length);
+        if(questionNumber>=(questionsQuiz1.length-1)){                          //END
+            console.log("NO NEXT?")
+            questionCount.innerText="---------";
+            questionSlot.innerText="Final score is: " + score;
+            // questionNumber++;
+        } else {                                                        //OTHERWISE NOT END
 
-    console.log(radioBtns);
+        
 
-    j=Math.floor(Math.random() * 3); 
-    for(let i=0; i<3;i++){
-        radioBtns[j%3].innerText=questionsQuiz1[questionNumber].options[i];
-        j++;
+        //GET THE INDEX OF PRESSED RADIO BUTTON
+
+        const data = new FormData(form);
+        let output = "";
+        for (const entry of data) {
+            console.log(radioBtns[entry[1].match(/(\d+)/)[0]-1].innerText);
+            console.log(questionsQuiz1[questionNumber].answer);
+        if(radioBtns[entry[1].match(/(\d+)/)[0]-1].innerText==questionsQuiz1[questionNumber].answer){
+            previousAnswer=questionsQuiz1[questionNumber].answer;
+            score++;
+        } else {
+            previousAnswer=questionsQuiz1[questionNumber].options[entry[1].match(/(\d+)/)[0]-1];
+        }
+        
+
+        console.log("inside nonfinale");
+        
+        
+        if (typeof questionsQuiz1[questionNumber+1].question!==undefined){
+            console.log("Inside inside");
+            questionNumber++;
+            questionCount.innerText=questionNumber+1;
+            questionSlot.innerText = questionsQuiz1[questionNumber].question;
+            j=Math.floor(Math.random() * 4); 
+            for(let i=0; i<4;i++){
+                
+                if(i!=3){
+                    radioBtns[j%4].innerText=questionsQuiz1[questionNumber].options[i];
+                    j++;
+                    console.log("Branch 1");
+                    
+                } else {
+                    radioBtns[j%4].innerText=questionsQuiz1[questionNumber].answer;
+                    console.log("Branch 2");
+                    j++;
+            
+                }
+        }       
+        
+
+        } 
+
+    
+
+    }
+    log.innerText = score;
+
+    console.log(score);
+    event.preventDefault();
+}
+}
+}
+
+
+
+
+
+const form = document.querySelector("form");
+const log = document.querySelector("#log");
+
+form.addEventListener(
+  "submit", init, /*
+
+  (event) => {
+    
+    const data = new FormData(form);
+    let output = "";
+    for (const entry of data) {
+      output = `${output}${entry[0]}=${entry[1]}\r`;
+    }
+    log.innerText = output;
+
+    
+    event.preventDefault(); 
+  }*/
+  false,
+);
+
+//initializes the default values:
+
+questionCount.innerText=1;
+questionSlot.innerText= questionsQuiz1[questionNumber].question;
+
+j=Math.floor(Math.random() * 4); 
+    for(let i=0; i<4;i++){
+        
+        if(i!=3){
+            radioBtns[j%4].innerText=questionsQuiz1[questionNumber].options[i];
+            j++;
+            console.log("Branch 1");
+            
+        } else {
+            radioBtns[j%4].innerText=questionsQuiz1[questionNumber].answer;
+              console.log("Branch 2");
+            j++;
+           }
     }
 
 
-}
+
+//shuffle variables only once at the beginning: saving a state
