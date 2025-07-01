00eb7ecb4813f659b56d37dac69428d6ff326f5b
Ebrahim User <ebrahimuo@gmail.com>
2025-07-01 10:51:11 +0300

    Added quiz1 questions and basic layout

---
+++ js/app.js
@@ -0,0 +1,74 @@
+/*-------------- Constants -------------*/
+
+const questionsQuiz1=[{question: "What was the first Pokémon ever created?",
+                 answer: "Rhydon",
+                 options: ["Pikachu", "Bulbasaur", "Mew"]
+                },{question: "Which Pokémon is known as the “Eon Pokémon”?",
+                    answer: " Latios and Latias",
+                    options: ["Lugia", "Mewtwo", "Rayquaza"]
+                },{question: "What type is the Pokémon “Garchomp”?",
+                    answer: "Dragon/Ground",
+                    options: ["Dragon/Flying", "Ground/Rock", "Dragon/Steel"]
+                },{question: "In which generation was the Fairy-type introduced?",
+                    answer: "Generation VI",
+                    options: ["Generation IV", "Generation V", "Generation VII"]
+                },{question: "Which of the following Pokémon is not a Legendary Pokémon?",
+                    answer: "Garchomp",
+                    options: ["Entei", "Regirock", "Cresselia"]
+                },{question: "What is the name of the region in Pokémon Sun and Moon?",
+                    answer: "Alola",
+                    options: ["Sinnoh", "Hoenn", "Unova"]
+                },{question: "Which Pokémon can evolve into multiple different forms depending on certain conditions?",
+                    answer: "Eevee",
+                    options: ["Pikachu", "Mew", "Ditto"]
+                },{question: "What move does the most damage to a Ghost-type Pokémon?",
+                    answer: "Crunch",
+                    options: ["Psychic", "Shadow Ball", "Hyper Beam"]
+                },{question: "Which Pokémon is known for having the signature move “Spacial Rend”?",
+                    answer: "Palkia",
+                    options: ["Dialga", "Giratina", "Arceus"]
+                },{question: "Who is the current champion of the Galar region in Pokémon Sword and Shield?",
+                    answer: "Leon",
+                    options: ["Hop", "Bea", "Raihan"]
+                }];
+
+const questionsQuiz2=[{question: "What was the first Pokémon ever created?",
+                 answer: "Rhydon",
+                 options: ["Pikachu", "Bulbasaur", "Mew"]
+                },{question: "Which Pokémon is known as the “Eon Pokémon”?",
+                    answer: " Latios and Latias",
+                    options: ["Lugia", "Mewtwo", "Rayquaza"]
+                },{question: "What type is the Pokémon “Garchomp”?",
+                    answer: "Dragon/Ground",
+                    options: ["Dragon/Flying", "Ground/Rock", "Dragon/Steel"]
+                },{question: "In which generation was the Fairy-type introduced?",
+                    answer: "Generation VI",
+                    options: ["Generation IV", "Generation V", "Generation VII"]
+                },{question: "Which of the following Pokémon is not a Legendary Pokémon?",
+                    answer: "Garchomp",
+                    options: ["Entei", "Regirock", "Cresselia"]
+                },{question: "What is the name of the region in Pokémon Sun and Moon?",
+                    answer: "Alola",
+                    options: ["Sinnoh", "Hoenn", "Unova"]
+                },{question: "Which Pokémon can evolve into multiple different forms depending on certain conditions?",
+                    answer: "Eevee",
+                    options: ["Pikachu", "Mew", "Ditto"]
+                },{question: "What move does the most damage to a Ghost-type Pokémon?",
+                    answer: "Crunch",
+                    options: ["Psychic", "Shadow Ball", "Hyper Beam"]
+                },{question: "Which Pokémon is known for having the signature move “Spacial Rend”?",
+                    answer: "Palkia",
+                    options: ["Dialga", "Giratina", "Arceus"]
+                },{question: "Who is the current champion of the Galar region in Pokémon Sword and Shield?",
+                    answer: "Leon",
+                    options: ["Hop", "Bea", "Raihan"]
+                }];
+
+
+/*---------- Variables (state) ---------*/
+
+
+/*----- Cached Element References  -----*/
+
+
+/*-------------- Functions -------------*/
