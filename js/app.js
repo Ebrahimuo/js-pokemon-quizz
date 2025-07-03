61a435223e730208331c3d25f41006df3774abe4
Ebrahim User <ebrahimuo@gmail.com>
2025-07-03 13:57:21 +0300

    Cleaned up some code.

--- js/app.js
+++ js/app.js
@@ -2,72 +2,77 @@
 
 //I don't know how to add animations... failed task
 
+// ---------------------------------------------------------------------
+
+// ^ Need to work on this
 
 /*-------------- CONS -------------*/
 
 import {questionsQuiz1,  questionsQuiz2} from "/js/dataFile.js";
 
-
-
 /*---------- VARS ---------*/
 
 let newGame=true;
 let questionNumber = 0;
-let trackingQuestion= 0;
 let score = 0;
 let previousAnswer="";
 let finished=false;
 let j;
 var questionsArr = [];
 var tryQuiz=0;
-var darkMode=false;
+
+var elementBdy = document.body;    //FOR TOGGLING DARK-MODE
 
 
-let turnToggle =0;
-
-var elementBdy = document.body;
+//                 AUDIO ELEMENTS
 
 const nextAudio = document.getElementById("nextAudio");
 const prevAudio = document.getElementById("prevAudio");
-const failAudio = document.getElementById("failAudio");
+const failAudio = document.getElementById("failAudio");             // ALL AUDIO ^ and <
 const successAudio = document.getElementById("successAudio");
 const mediumAudio = document.getElementById("mediumAudio");
 
 /*-----     CACHE  -----*/
 
-let questionCount = document.getElementById("questionN");
-let questionSlot = document.getElementById("questionSlot");
 
-
-let radioWrap = document.getElementById("radioWrap");
-
-let radioBtns = document.querySelectorAll(".form-check-label");
+let radioBtns = document.querySelectorAll(".form-check-label");     
 const formInputs = document.querySelectorAll("form-check-input");
 
-let prevButton = document.getElementById("prevButton");
-let proceedButton = document.getElementById("proceedButton");
+let answerText = document.getElementById("answerText");   // ANSWER LOCATION
+let questionCount = document.getElementById("questionN");  // QUESTION COUNT AMOUNT
+let questionSlot = document.getElementById("questionSlot"); // QUESTION LOCATION
+let quizLayout = document.getElementById("quizLayout");   // QUIZ CARD
+
+let quizzPlayBtns = document.getElementsByClassName("quizzPlayBtns") ;  // PLAY BUTTONS FOR QUIZZ
+let prevButton = document.getElementById("prevButton");               // PREVIOUS BUTTON
+let proceedButton = document.getElementById("proceedButton");         // NEXT BUTTON   
+let toggleBtn= document.getElementById("toggleButton");  //LIGHT AND DARK MODE BUTTON
+let rubberBand= document.getElementById("rubberBand");   //TITLE
 
 
-let answerText = document.getElementById("answerText");
-
-let quizLayout = document.getElementById("quizLayout");
+let radioWrap = document.getElementById("radioWrap");      //RADIO DIV CONTAINER     
 
 
-let quizzPlayBtns = document.getElementsByClassName("quizzPlayBtns") ;
+const form = document.querySelector("form");
+const log = document.querySelector("#log");
+
+if(form ) {
+form.addEventListener("submit", init,  false);
+}
 
 /*-------------- FUNCTIONS -------------*/
 
 
 function init(event){
-    event.preventDefault(); //allows the final page
+    event.preventDefault(); //ALLOWS the final page to work.
 
-    if(finished&& event.submitter.id=="prevButton"){           //RESTART IS PRESSED
-        //no audio to play here
-        console.log("At first if");
+    if(finished && event.submitter.id=="proceedButton") {            //FINISHED AND PROCEED ARE PRESSED
+        //redirect page  
+        window.location.href = `quiz${tryQuiz}.html`;
 
+    } else if(finished&& event.submitter.id=="prevButton"){           //RESTART IS PRESSED
         newGame=true;
-        questionNumber = 0;
-        trackingQuestion= 0;
+        questionNumber = 0;;
         score = 0;
         previousAnswer="";
         finished=false;
@@ -89,23 +94,21 @@ function init(event){
                     radioBtns[j%4].innerText=questionsArr[questionNumber].answer;
                     j++;
            }
-      } 
+    } 
             questionNumber++;
             radioWrap.style.display="block";
             prevButton.innerText="Previous!";
             finished=false;
             proceedButton.innerText="Next";
-    }
-    if(event.submitter.id=="prevButton"){ // RUN ONLY ON PREVIOUS BUTTON
-        console.log("At previous");
+            rubberBand.innerText="Welcome to Pokémon Quiz!";
+
+    } else if(event.submitter.id=="prevButton"){            //PREVIOUS BUTTON IS PRESSED AND NOT FINISHED
         if (previousAnswer==questionsArr[questionNumber].answer){
             score--;
             previousAnswer="";
         }
         questionNumber--;
-        //animate
         questionCount.innerText="#"+ (questionNumber+1) ;
-        //animate
          questionSlot.innerText = questionsArr[questionNumber].question;
          for(let i=0; i<4;i++){
         j=Math.floor(Math.random() * 4); 
@@ -118,16 +121,10 @@ function init(event){
             radioBtns[j%4].innerText=questionsArr[questionNumber].answer;
             j++;
            }
-    }    
+        }    
 
-    }  else if(finished && event.submitter.id=="proceedButton") {
-        //redirect page  
-        window.location.href = `quiz${tryQuiz}.html`;
-
-    } else {//NEXT BUTTON PRESS
+    }  else {                                                //NEXT BUTTON PRESS OR FORM COMPLETION
        
-
-            
         if(event.submitter.id=="proceedButton" &&!finished){
             nextAudio.play();
         } else if(!finished) {
@@ -136,9 +133,8 @@ function init(event){
         fadeInRadio();
         fadeinQuiz();
         animateTitle();
-        
 
-        if(questionNumber==(questionsArr.length-1)){                          //END END END
+        if(questionNumber==(questionsArr.length-1)){                          //FORM COMPLETION
             questionCount.innerText="----E N D-----";
             if(score>7){
                 successAudio.play();
@@ -152,66 +148,49 @@ function init(event){
             answerText.innerText="";
             radioWrap.style.display="none";
             prevButton.innerText="Restart!";
-            //not quite the
             finished=true;
             proceedButton.innerText=`Try Quizz ${tryQuiz}?`;
+            rubberBand.innerText="Quizz Completed";
             
-        } else if( questionNumber<(questionsArr.length-1)){                                  //OTHERWISE NOT END
+        } else if( questionNumber<(questionsArr.length-1)){   //NORMAL NEXT PROCEEDURE
 
-        //GET THE INDEX OF PRESSED RADIO BUTTON
 
-        const data = new FormData(form);
-        let output = "";
-        for (const entry of data) {
-        if(radioBtns[entry[1].match(/(\d+)/)[0]-1].innerText==questionsArr[questionNumber].answer){
-            previousAnswer=questionsArr[questionNumber].answer;
-            score++;
-        } else {
-            previousAnswer=questionsArr[questionNumber].options[entry[1].match(/(\d+)/)[0]-1];
-        }
-        
-        
-        if (typeof questionsArr[questionNumber+1].question!==undefined){
-            questionNumber++;
-            questionCount.innerText="#" + (questionNumber+1) ;
-            questionSlot.innerText = questionsArr[questionNumber].question;
-            j=Math.floor(Math.random() * 4); 
-            fadeInRadio();
-            fadeinQuiz();
-             animateTitle();
-            for(let i=0; i<4;i++){
-                
-                if(i!=3){
-                    radioBtns[j%4].innerText=questionsArr[questionNumber].options[i];
-                    j++;
-                    
-                } else {
-                    radioBtns[j%4].innerText=questionsArr[questionNumber].answer;
-                    j++;
+            //GET THE INDEX OF PRESSED RADIO BUTTON
+            const data = new FormData(form);
+            let output = "";
+            for (const entry of data) {
+            if(radioBtns[entry[1].match(/(\d+)/)[0]-1].innerText==questionsArr[questionNumber].answer){
+                previousAnswer=questionsArr[questionNumber].answer;
+                score++;
+            } else {
+                previousAnswer=questionsArr[questionNumber].options[entry[1].match(/(\d+)/)[0]-1];
+            }
             
-                }
-        }       
-        } 
+            if (typeof questionsArr[questionNumber+1].question!==undefined){
+                questionNumber++;
+                questionCount.innerText="#" + (questionNumber+1) ;
+                questionSlot.innerText = questionsArr[questionNumber].question;
+                j=Math.floor(Math.random() * 4); 
+                fadeInRadio();
+                fadeinQuiz();
+                animateTitle();
+                for(let i=0; i<4;i++){
+                    if(i!=3){
+                        radioBtns[j%4].innerText=questionsArr[questionNumber].options[i];
+                        j++;
+                    } else {
+                        radioBtns[j%4].innerText=questionsArr[questionNumber].answer;
+                        j++;
+                    }
+                }       
+            } 
+            }
         }
-        }
-        else {
+        else {                                  //SHOULD NEVER BE HERE.
             console.log("Error Area.");
         }
-
     }
 }
-
-const form = document.querySelector("form");
-const log = document.querySelector("#log");
-
-if(form ) {
-form.addEventListener("submit", init,  false);
-}
-
-// ON LOAD INITIALIZATION
-
-
-
 
 function fadeInRadio(){
     radioBtns.forEach(element => {
@@ -232,8 +211,6 @@ function fadeinQuiz(){
     }
 
 
-let rubberBand= document.getElementById("rubberBand");
-
 function animateTitle(){
     setTimeout(() => {
         rubberBand.classList.toggle('animate__rubberBand');
@@ -242,20 +219,20 @@ function animateTitle(){
     }
 
 
-let toggleBtn= document.getElementById("toggleButton");
+
 
 toggleBtn.addEventListener('click', toggleD);
 
 function toggleD() {
   var elementBdy = document.body;
   elementBdy.classList.toggle("dark-mode");
-  let currentBGC= document.getElementsByClassName("navbar")[0].style.backgroundColor
-  console.log("Background color is: " + currentBGC);
+  
+//   let currentBGC= document.getElementsByClassName("navbar")[0].style.backgroundColor
+//   console.log("Background color is: " + currentBGC);
+
   const darkMode = localStorage.getItem("darkMode");
   
   if(darkMode=="false"){
-    console.log("inside if 1 toggleD");
-    
     document.getElementsByClassName("navbar")[0].style.setProperty('background-color', '#262626', 'important');
     document.getElementsByTagName("a")[0].style.setProperty('color', 'white', 'important');
     document.getElementsByTagName("a")[1].style.setProperty('color', 'white', 'important');
@@ -272,27 +249,21 @@ function toggleD() {
         proceedButton.style.setProperty('background-color', "#9e9e9e", 'important');
         proceedButton.style.setProperty('border', "1px black solid", 'important');
     }
-
-    
     if (quizzPlayBtns[0]) {
         quizzPlayBtns[0].style.setProperty('background-color', "#9e9e9e", 'important');
          quizzPlayBtns[0].style.setProperty('border', "1px black solid", 'important');
         quizzPlayBtns[1].style.setProperty('background-color', "#9e9e9e", 'important');
          quizzPlayBtns[1].style.setProperty('border', "1px black solid", 'important');
     }
-
     if(prevButton) {
         prevButton.style.setProperty('color', 'white', 'important');
         prevButton.style.setProperty('background-color', "#9e9e9e", 'important');
         prevButton.style.setProperty('border', "1px black solid", 'important');
     }
-    
     localStorage.setItem("darkMode", true);
-    console.log("Darkmode value is: " + localStorage.getItem("darkMode"));
 
   } else {
-    console.log("inside if 2 toggleD")
-    
+
     document.getElementsByClassName("navbar")[0].style.setProperty('background-color', 'white', 'important');
     document.getElementsByTagName("a")[0].style.setProperty('color', 'black', 'important');
     document.getElementsByTagName("a")[1].style.setProperty('color', 'black', 'important');
@@ -310,41 +281,31 @@ function toggleD() {
         quizzPlayBtns[1].style.setProperty('background-color', "blue", 'important');
          quizzPlayBtns[1].style.setProperty('border', "1px black solid", 'important');
     }
-
     if(proceedButton){
             proceedButton.style.setProperty('color', 'white', 'important');
             proceedButton.style.setProperty('background-color', "blue", 'important');
             proceedButton.style.setProperty('border', "1px black solid", 'important');
         }
-
         if(prevButton){
             prevButton.style.setProperty('color', 'white', 'important');
             prevButton.style.setProperty('background-color', "blue", 'important');
             prevButton.style.setProperty('border', "1px black solid", 'important');
-
         }
-    
-
     localStorage.setItem("darkMode", false);
-    console.log("Darkmode value is: " + darkMode);
   }
 }
 
+//ON PAGE LOAD
 
-
-//on.load apply appropriate theme:
 window.onload = function() {
     const darkMode = localStorage.getItem("darkMode");
-    console.log("Darkmode value is: " + darkMode);
-
     if (darkMode==undefined){
         localStorage.setItem("darkMode", false);
     }
 
-    if(darkMode=="true"){
+    if(darkMode=="true"){                         // DARKMODE IS ENABLED
         //below is dark mode
-        console.log("Onload if 1 and darkmode is: " + darkMode);
-        elementBdy.classList.add("dark-mode");
+        elementBdy.classList.add("dark-mode");   //IN TOGGLE INSTANCE IT'S JUST.., TOGGLE.
         document.getElementsByClassName("navbar")[0].style.setProperty('background-color', '#262626', 'important');
         document.getElementsByTagName("a")[0].style.setProperty('color', 'white', 'important');
         document.getElementsByTagName("a")[1].style.setProperty('color', 'white', 'important');
@@ -376,8 +337,7 @@ window.onload = function() {
     }
 
         
-    } else {
-        console.log("inside if 2 and darkmode is " + darkMode);
+    } else {                                                //DARKMODE IS DISABLED
         elementBdy.classList.remove("dark-mode");
         document.getElementsByClassName("navbar")[0].style.setProperty('background-color', 'white', 'important');
         document.getElementsByTagName("a")[0].style.setProperty('color', 'black', 'important');
@@ -414,39 +374,35 @@ window.onload = function() {
   }
 
 
-  //
+  // INITIALIZE AND REPLACE HTML ON FIRST LOADING.
 
-     if (window.location.pathname === "/quiz1.html") {
-        questionsArr = questionsQuiz1;
-        tryQuiz=2;
-    } else if (window.location.pathname === "/quiz2.html") {
-        questionsArr = questionsQuiz2;
-        tryQuiz=1;
-    }
-        questionCount.innerText="#1";
-        questionSlot.innerText= questionsArr[questionNumber].question;
-            
-        j=Math.floor(Math.random() * 4); 
-            for(let i=0; i<4;i++){
-                
-                if(i!=3){
-                    radioBtns[j%4].innerText=questionsArr[questionNumber].options[i];
-                    j++;
+    if (window.location.pathname === "/quiz1.html" || window.location.pathname === "/quiz2.html"){
+        if (window.location.pathname === "/quiz1.html") {
+            questionsArr = questionsQuiz1;
+            tryQuiz=2;
+        } else if (window.location.pathname === "/quiz2.html") {
+            questionsArr = questionsQuiz2;
+            tryQuiz=1;
+        }
+            questionCount.innerText="#1";
+            questionSlot.innerText= questionsArr[questionNumber].question;
+            j=Math.floor(Math.random() * 4); 
+                for(let i=0; i<4;i++){
                     
-                } else {
-                    radioBtns[j%4].innerText=questionsArr[questionNumber].answer;
-                    j++;
+                    if(i!=3){
+                        radioBtns[j%4].innerText=questionsArr[questionNumber].options[i];
+                        j++;
+                        
+                    } else {
+                        radioBtns[j%4].innerText=questionsArr[questionNumber].answer;
+                        j++;
+                    }
                 }
-            }
-        fadeInRadio();
-            
-        fadeinQuiz();
-
-        console.log(radioBtns);
+            fadeInRadio();
+            fadeinQuiz();;
+    }
     
 }
 
-console.log("Darkmode value is: " + localStorage.getItem("darkMode"));
 
 
-// currentModeCheck(); 
